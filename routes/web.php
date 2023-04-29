bbf90ea94c336aa436f09bab48c84ee01386306e
root <root@iqbalfaris.my.id>
2023-04-29 17:27:27 +0000

    from vps

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes by Laravel
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
